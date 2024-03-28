efb3de3fb416c0aedc5bd5c19b609df1c1374885
hyoyoungjjang <khy20030930@gmail.com>
2024-03-28 17:56:14 +0900

    beakjoon

--- baekjoon/src/baekjoon/Main.java
+++ baekjoon/src/baekjoon/Main.java
@@ -1,63 +1,11 @@
 package baekjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 
 public class Main {
-	public static char[][] arr;
-	public static  int resultMin = 2501;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		arr = new char[n][m];
-		String s;
+	public static void main(String[] args) {
 		
+	}
 		
-		for(int i = 0; i < n; i++) {
-			s = br.readLine();
-			for(int j = 0; j < m; j++) {
-				arr[i][j] = s.charAt(j);
-			}
-		}						
-		for(int z = 0; z <= arr.length-8; z++) {				
-			for(int x =0; x <= arr[0].length-8; x++) {
-				resultMin = Math.min(resultMin, getArr(z,x));
-			}
-		}
-		System.out.println(resultMin);	
-	}
-	
-	
-	public static int getArr(int row, int col) {
-		int resutl = 0;
-		int black = 0;
-		int white = 0;
-		for(int i =0; i < 8; i++) {
-			for(int j =0; j < 8; j++) {
-				if((i+j)%2==0) {
-					if(arr[row+i][col+j] == 'B') {
-						white++;		
-					}else {
-						black++;
-					}
-				}else {
-					if(arr[row+i][col+j] == 'W') {
-						white++;
-					}else {
-						black++;
-					}	
-				}
-			}
-		}
-		resutl = Math.min(black, white);	
-		return resutl;
-	}
 }
 
 
---
+++ baekjoon/src/baekjoon/큐2.java
@@ -0,0 +1,58 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class 큐2 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		Deque<Integer> d = new LinkedList<>();
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			String stt = st.nextToken();
+			if(stt.equals("push")) {
+				d.add(Integer.parseInt(st.nextToken()));
+			} else if (stt.equals("pop")) {
+				if(d.isEmpty()) {
+					bw.write(-1 + "\n");
+				}else {
+					bw.write(d.pop() + "\n");
+				}
+			} else if(stt.equals("size")){
+				bw.write(d.size() + "\n");	
+			} else if(stt.equals("empty")) {
+				if(d.isEmpty()) {
+					bw.write(1 + "\n");
+				} else {
+					bw.write(0 + "\n");
+				}
+			} else if(stt.equals("front")) {
+				if(d.isEmpty()) {
+					bw.write(-1 + "\n");
+				}else {
+					bw.write(d.getFirst() + "\n");
+				}
+			} else if(stt.equals("back")) {
+				if(d.isEmpty()) {
+					bw.write(-1 + "\n");
+				}else {
+					bw.write(d.getLast() + "\n");
+				}
+			}
+			
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
