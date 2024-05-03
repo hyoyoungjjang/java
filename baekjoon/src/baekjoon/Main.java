b7bd25ff9b3cc9fcd13234fc5340f999731407ad
hyoyoungjjang <khy20030930@gmail.com>
2024-05-03 17:51:16 +0900

    ..

--- baekjoon/src/baekjoon/Main.java
+++ baekjoon/src/baekjoon/Main.java
@@ -1,24 +1,47 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb = new StringBuilder();
+	static int count = 0;
 	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		int v = Integer.parseInt(st.nextToken());
-		
-		
-		for(int i = 0; i < m; i++) {
-			
+		int n = Integer.parseInt(st.nextToken()); 
+		int first = 1;
+		int two = 2;
+		int three = 3;
+		hanoi(n,first, two, three);
+		bw.write(count + "\n");
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	
+	public static void hanoi(int n, int a, int b, int c) throws IOException {
+	
+		if(n == 1) {
+			count++;
+			sb.append(a + " " + c + "\n");
+			return;
+		} else {
+			hanoi(n-1,a,c,b);
+			hanoi(1,a,b,c);
+			hanoi(n-1,b,a,c);
 		}
-	}	
+		
+	}
+	
 }
+
+
 
 
